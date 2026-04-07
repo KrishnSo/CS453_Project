968dd4e0cc765a6cfa567be2901ee09dee4c988e
Krishna Pratheek Somarapu <ksomar3@uic.edu>
2026-04-07 14:55:41 -0500

    Working MPI leader election and dijkstra prototype

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,131 @@
 #include <mpi.h>
 #include <iostream>
+#include <vector>
+#include <limits>
 
-#define PING_PONG_LIMIT 10
+using namespace std;
+
+struct Node {
+    int id;
+    vector<pair<int,int>> neighbors;
+};
+
+vector<Node> create_graph() {
+    vector<Node> g(6);
+    for(int i = 0; i < 6; i++) g[i].id = i;
+
+    g[0].neighbors = {{1,1}, {2,4}};
+    g[1].neighbors = {{0,1}, {3,2}};
+    g[2].neighbors = {{0,4}, {3,1}};
+    g[3].neighbors = {{1,2}, {2,1}, {4,3}};
+    g[4].neighbors = {{3,3}, {5,2}};
+    g[5].neighbors = {{4,2}};
+
+    return g;
+}
+
+vector<Node> get_local_nodes(vector<Node>& graph, int rank, int size) {
+    vector<Node> local;
+    for(auto &n : graph) {
+        if(n.id % size == rank) {
+            local.push_back(n);
+        }
+    }
+    return local;
+}
+
+void leader_election(vector<Node>& graph, vector<Node>& local_nodes, int rank) {
+    vector<int> leader(6);
+    for(int i = 0; i < 6; i++) leader[i] = i;
+
+    for(int round = 0; round < 5; round++) {
+        vector<int> new_leader = leader;
+
+        for(auto &n : local_nodes) {
+            for(auto &e : n.neighbors) {
+                int neighbor = e.first;
+                new_leader[n.id] = max(new_leader[n.id], leader[neighbor]);
+            }
+        }
+
+        MPI_Allreduce(new_leader.data(), leader.data(), 6,
+                      MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+    }
+
+    if(rank == 0) {
+        cout << "Leader: " << leader[0] << endl;
+    }
+}
+
+void dijkstra(vector<Node>& graph, vector<Node>& local_nodes, int rank) {
+    const int INF = 1e9;
+    vector<int> dist(6, INF);
+    vector<bool> visited(6, false);
+
+    dist[0] = 0;
+
+    for(int i = 0; i < 6; i++) {
+
+        int local_min = INF;
+        int local_node = -1;
+
+        for(auto &n : local_nodes) {
+            if(!visited[n.id] && dist[n.id] < local_min) {
+                local_min = dist[n.id];
+                local_node = n.id;
+            }
+        }
+
+        struct {
+            int dist;
+            int node;
+        } local_pair = {local_min, local_node}, global_pair;
+
+        MPI_Allreduce(&local_pair, &global_pair, 1,
+                      MPI_2INT, MPI_MINLOC, MPI_COMM_WORLD);
+
+        int u = global_pair.node;
+        if(u == -1) break;
+
+        visited[u] = true;
+
+        for(auto &e : graph[u].neighbors) {
+            int v = e.first;
+            int w = e.second;
+
+            if(dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+            }
+        }
+
+        MPI_Allreduce(MPI_IN_PLACE, dist.data(), 6,
+                      MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+    }
+
+    if(rank == 0) {
+        cout << "Distances:\n";
+        for(int i = 0; i < 6; i++) {
+            cout << i << " -> " << dist[i] << endl;
+        }
+    }
+}
 
 int main(int argc, char** argv) {
-    // Initialize the MPI environment
     MPI_Init(&argc, &argv);
 
-    // Get the number of processes
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Get the rank of the process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    auto graph = create_graph();
+    auto local_nodes = get_local_nodes(graph, rank, size);
 
-    // We need exactly 2 processes for ping-pong
-    if (world_size != 2) {
-        if (world_rank == 0) {
-            std::cerr << "Error: This application requires exactly 2 MPI processes." << std::endl;
-            std::cerr << "Usage: mpirun -np 2 ./ping_pong" << std::endl;
-        }
-        MPI_Finalize();
-        return 1;
-    }
+    if(rank == 0) cout << "Running Leader Election...\n";
+    leader_election(graph, local_nodes, rank);
 
-    int ping_pong_count = 0;
-    int partner_rank = (world_rank + 1) % 2;
-    
-    while (ping_pong_count < PING_PONG_LIMIT) {
-        if (world_rank == ping_pong_count % 2) {
-            // Increment the ping pong count before sending
-            ping_pong_count++;
-            
-            // Send the count to the partner
-            MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
-            
-            std::cout << "Process " << world_rank << " sent ping_pong_count " 
-                      << ping_pong_count << " to process " << partner_rank << std::endl;
-        } else {
-            // Receive the count from the partner
-            MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0, 
-                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            
-            std::cout << "Process " << world_rank << " received ping_pong_count " 
-                      << ping_pong_count << " from process " << partner_rank << std::endl;
-        }
-    }
+    if(rank == 0) cout << "\nRunning Dijkstra...\n";
+    dijkstra(graph, local_nodes, rank);
 
-    std::cout << "Process " << world_rank << " finished ping-pong with count " 
-              << ping_pong_count << std::endl;
-
-    // Finalize the MPI environment
     MPI_Finalize();
     return 0;
 }
